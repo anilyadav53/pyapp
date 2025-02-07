e05a77819cac8c57e2717f0c01b63c5482cd0df0
Harsh <anilyaduvansi17799@gmail.com>
2025-02-07 12:37:31 +0530

    update 3

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, DevOps Engineer!"
+    return "Hello, How are you Mr. Engineer!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000) 
