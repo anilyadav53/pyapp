93b07a4ba4495eafb6c4e0090d4dd6d8e8ffbf98
Harsh <anilyaduvansi17799@gmail.com>
2025-02-09 17:13:12 +0530

    update kubernetes files

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Mr. what a lovely day!  This is jenkins automated pipeline"
+    return "Hello, Mr. what a lovely day!  This is jenkins automated pipeline for our first project."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000) 
