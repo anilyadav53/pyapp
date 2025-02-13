4d4fb88f002fc6846ed7cbff495c0ed1aa62ead5
Harsh <anilyaduvansi17799@gmail.com>
2025-02-13 12:03:11 +0530

    change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello,  Mr. Engineer What a lovely day!  This is jenkins Automated pipeline."
+    return "Hello,  Mr. Engineer lovely day!  This is jenkins Automated pipeline."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000) 
